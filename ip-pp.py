04e336b4748d42734f6b62e430ac5c09e98ced8a
Balta <79738875+Balta-Python@users.noreply.github.com>
2021-03-23 11:42:04 -0700

    Update ip-pp.py

--- ip-pp.py
+++ ip-pp.py
@@ -9,8 +9,8 @@ import os
 
 os.system("clear")
 
-loop = tqdm(total=30000, position=0, leave=False)
-for k in range(30000):
+loop = tqdm(total=10000, position=0, leave=False)
+for k in range(10000):
     loop.set_description(Fore.LIGHTRED_EX + 'Opening Script'.format(k))
     loop.update(1)
 loop.close()
